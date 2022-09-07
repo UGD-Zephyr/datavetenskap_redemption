e6fe9415b0686f285ed13898f045c9c5436fad8d
Zephyr <ugd.zephyr@gmail.com>
2022-09-07 14:38:36 +0200

    Commiting my redemption arc.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Datavetenskap Redemption
+
+Once upon a time I was a computer science dropout.
+This repository will be my redemption.
+
+I will complete the programming assignments I was
+given all those years ago and COMPLETE them!
+
+- The first one is a Celsius / Fahrentheit converter
+- The second one is something with arrays...don't quite remember yet..
+- The third one is a .txt file with data on people. Names, age, work
+  where you also can search for any one, individual data and get all
+  corresponding data as well.
---
+++ struct_list.c
@@ -0,0 +1,124 @@
+/* Programmer: Per Stoor
+ * Date: 2022-08-24
+ * Last changed: 2022-08-24
+ * Type of program: Enter data about various persons,
+ * age, weight, name, job and so on and save it in a
+ * .txt text file. 
+ * Also let the user search this file among various 
+ * data.
+ *
+ * Notes:
+ * */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>            //termios, TCSANOW, ECHO, ICANON
+#include <unistd.h>             //STDIN_FILENO
+
+#define STRING_LENGTH 50
+#define MENU_ARROW_TOP 1
+#define MENU_ARROW_BOTTOM 2
+
+void introduction_function();
+void menu_arrow_function(int current_arrow_position, int future_arrow_position);
+int main_menu_function();
+
+typedef struct{
+
+	int age;
+	int weight;
+	char job[STRING_LENGTH];
+	char first_name[STRING_LENGTH];
+	char last_name[STRING_LENGTH];
+
+} person;
+
+int main(int argc, char *argv[]){
+
+	int user_choice;
+	static struct termios oldt, newt;
+
+	/*tcgetattr gets the parameters of the current terminal
+	STDIN_FILENO will tell tcgetattr that it should write the settings
+	of stdin to oldt*/
+	tcgetattr( STDIN_FILENO, &oldt);
+	/*now the settings will be copied*/
+	newt = oldt;
+	/*ICANON normally takes care that one line at a time will be processed
+	that means it will return if it sees a "\n" or an EOF or an EOL*/
+	newt.c_lflag &= ~(ICANON);
+	/*Those new settings will be set to STDIN
+	CSANOW tells tcsetattr to change attributes immediately. */
+	tcsetattr( STDIN_FILENO, TCSANOW, &newt);
+
+		introduction_function();
+		user_choice = main_menu_function();
+
+			if(user_choice == 1){
+				system("clear");
+				printf("SAVE DATA TO FILE");
+				printf("\n");
+			}
+			else if(user_choice == 2){
+				system("clear");
+				printf("SEARCH DATA FROM FILE");
+				printf("\n");
+			}
+
+	/*restore the old settings*/
+	tcsetattr( STDIN_FILENO, TCSANOW, &oldt);
+
+return 0;
+}
+
+void introduction_function(){
+
+	system("clear");
+	printf("HELLO! I AM THE INTRODUCTORY TEXT...\n");
+}
+
+int main_menu_function(){
+
+	int function_loop_counter1;
+	int menu_arrow_position;
+	int inputted_keyboard_value;
+
+		menu_arrow_position = 2;
+		inputted_keyboard_value = 0;
+
+		printf("EXPLANATORY TEXT...\n");
+
+			while(inputted_keyboard_value != 10){
+
+				system("clear");
+				menu_arrow_function(1, menu_arrow_position); printf("1. SAVE DATA TO FILE\n");
+				menu_arrow_function(2, menu_arrow_position); printf("2. SEARCH DATA FROM FILE\n");
+
+				system("stty -echo");
+				inputted_keyboard_value = getchar();
+				system("stty echo");
+
+					if(inputted_keyboard_value == 65 && menu_arrow_position != MENU_ARROW_TOP){
+						menu_arrow_position--;
+					}
+					else if(inputted_keyboard_value == 66 && menu_arrow_position != MENU_ARROW_BOTTOM){
+						menu_arrow_position++;
+					}
+					else{
+						menu_arrow_position = menu_arrow_position;
+					}
+			}
+
+return menu_arrow_position;
+}
+
+void menu_arrow_function(int current_arrow_position, int future_arrow_position){
+
+	if(current_arrow_position == future_arrow_position){
+		printf("---> - ");
+	}
+	else{
+		printf("       ");
+	}
+
+}
